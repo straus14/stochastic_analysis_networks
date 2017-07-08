c59c7871566856472954ab297c6ae8ffda5e237d
Sudarshan <straus14straus14@gmail.com>
2017-07-08 20:16:25 +0200

    lab 1

---
+++ Lab01/src/bitmessage_m.cc
@@ -0,0 +1,291 @@
+//
+// Generated file, do not edit! Created by nedtool 4.6 from bitmessage.msg.
+//
+
+// Disable warnings about unused variables, empty switch stmts, etc:
+#ifdef _MSC_VER
+#  pragma warning(disable:4101)
+#  pragma warning(disable:4065)
+#endif
+
+#include <iostream>
+#include <sstream>
+#include "bitmessage_m.h"
+
+USING_NAMESPACE
+
+
+// Another default rule (prevents compiler from choosing base class' doPacking())
+template<typename T>
+void doPacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doPacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+template<typename T>
+void doUnpacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doUnpacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+
+
+
+// Template rule for outputting std::vector<T> types
+template<typename T, typename A>
+inline std::ostream& operator<<(std::ostream& out, const std::vector<T,A>& vec)
+{
+    out.put('{');
+    for(typename std::vector<T,A>::const_iterator it = vec.begin(); it != vec.end(); ++it)
+    {
+        if (it != vec.begin()) {
+            out.put(','); out.put(' ');
+        }
+        out << *it;
+    }
+    out.put('}');
+    
+    char buf[32];
+    sprintf(buf, " (size=%u)", (unsigned int)vec.size());
+    out.write(buf, strlen(buf));
+    return out;
+}
+
+// Template rule which fires if a struct or class doesn't have operator<<
+template<typename T>
+inline std::ostream& operator<<(std::ostream& out,const T&) {return out;}
+
+Register_Class(bitmessage);
+
+bitmessage::bitmessage(const char *name, int kind) : ::cMessage(name,kind)
+{
+    this->source_var = 0;
+}
+
+bitmessage::bitmessage(const bitmessage& other) : ::cMessage(other)
+{
+    copy(other);
+}
+
+bitmessage::~bitmessage()
+{
+}
+
+bitmessage& bitmessage::operator=(const bitmessage& other)
+{
+    if (this==&other) return *this;
+    ::cMessage::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void bitmessage::copy(const bitmessage& other)
+{
+    this->source_var = other.source_var;
+}
+
+void bitmessage::parsimPack(cCommBuffer *b)
+{
+    ::cMessage::parsimPack(b);
+    doPacking(b,this->source_var);
+}
+
+void bitmessage::parsimUnpack(cCommBuffer *b)
+{
+    ::cMessage::parsimUnpack(b);
+    doUnpacking(b,this->source_var);
+}
+
+bool bitmessage::getSource() const
+{
+    return source_var;
+}
+
+void bitmessage::setSource(bool source)
+{
+    this->source_var = source;
+}
+
+class bitmessageDescriptor : public cClassDescriptor
+{
+  public:
+    bitmessageDescriptor();
+    virtual ~bitmessageDescriptor();
+
+    virtual bool doesSupport(cObject *obj) const;
+    virtual const char *getProperty(const char *propertyname) const;
+    virtual int getFieldCount(void *object) const;
+    virtual const char *getFieldName(void *object, int field) const;
+    virtual int findField(void *object, const char *fieldName) const;
+    virtual unsigned int getFieldTypeFlags(void *object, int field) const;
+    virtual const char *getFieldTypeString(void *object, int field) const;
+    virtual const char *getFieldProperty(void *object, int field, const char *propertyname) const;
+    virtual int getArraySize(void *object, int field) const;
+
+    virtual std::string getFieldAsString(void *object, int field, int i) const;
+    virtual bool setFieldAsString(void *object, int field, int i, const char *value) const;
+
+    virtual const char *getFieldStructName(void *object, int field) const;
+    virtual void *getFieldStructPointer(void *object, int field, int i) const;
+};
+
+Register_ClassDescriptor(bitmessageDescriptor);
+
+bitmessageDescriptor::bitmessageDescriptor() : cClassDescriptor("bitmessage", "cMessage")
+{
+}
+
+bitmessageDescriptor::~bitmessageDescriptor()
+{
+}
+
+bool bitmessageDescriptor::doesSupport(cObject *obj) const
+{
+    return dynamic_cast<bitmessage *>(obj)!=NULL;
+}
+
+const char *bitmessageDescriptor::getProperty(const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? basedesc->getProperty(propertyname) : NULL;
+}
+
+int bitmessageDescriptor::getFieldCount(void *object) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? 1+basedesc->getFieldCount(object) : 1;
+}
+
+unsigned int bitmessageDescriptor::getFieldTypeFlags(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeFlags(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static unsigned int fieldTypeFlags[] = {
+        FD_ISEDITABLE,
+    };
+    return (field>=0 && field<1) ? fieldTypeFlags[field] : 0;
+}
+
+const char *bitmessageDescriptor::getFieldName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static const char *fieldNames[] = {
+        "source",
+    };
+    return (field>=0 && field<1) ? fieldNames[field] : NULL;
+}
+
+int bitmessageDescriptor::findField(void *object, const char *fieldName) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    int base = basedesc ? basedesc->getFieldCount(object) : 0;
+    if (fieldName[0]=='s' && strcmp(fieldName, "source")==0) return base+0;
+    return basedesc ? basedesc->findField(object, fieldName) : -1;
+}
+
+const char *bitmessageDescriptor::getFieldTypeString(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeString(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static const char *fieldTypeStrings[] = {
+        "bool",
+    };
+    return (field>=0 && field<1) ? fieldTypeStrings[field] : NULL;
+}
+
+const char *bitmessageDescriptor::getFieldProperty(void *object, int field, const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldProperty(object, field, propertyname);
+        field -= basedesc->getFieldCount(object);
+    }
+    switch (field) {
+        default: return NULL;
+    }
+}
+
+int bitmessageDescriptor::getArraySize(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getArraySize(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    bitmessage *pp = (bitmessage *)object; (void)pp;
+    switch (field) {
+        default: return 0;
+    }
+}
+
+std::string bitmessageDescriptor::getFieldAsString(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldAsString(object,field,i);
+        field -= basedesc->getFieldCount(object);
+    }
+    bitmessage *pp = (bitmessage *)object; (void)pp;
+    switch (field) {
+        case 0: return bool2string(pp->getSource());
+        default: return "";
+    }
+}
+
+bool bitmessageDescriptor::setFieldAsString(void *object, int field, int i, const char *value) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->setFieldAsString(object,field,i,value);
+        field -= basedesc->getFieldCount(object);
+    }
+    bitmessage *pp = (bitmessage *)object; (void)pp;
+    switch (field) {
+        case 0: pp->setSource(string2bool(value)); return true;
+        default: return false;
+    }
+}
+
+const char *bitmessageDescriptor::getFieldStructName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    switch (field) {
+        default: return NULL;
+    };
+}
+
+void *bitmessageDescriptor::getFieldStructPointer(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructPointer(object, field, i);
+        field -= basedesc->getFieldCount(object);
+    }
+    bitmessage *pp = (bitmessage *)object; (void)pp;
+    switch (field) {
+        default: return NULL;
+    }
+}
+
+
---
+++ Lab01/src/xorGate.cc
@@ -0,0 +1,43 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "xorGate.h"
+
+Define_Module(XorGate);
+
+void XorGate::initialize()
+{
+    in1 = false;
+    in2 = false;
+    out = false;
+    count = 0;
+}
+
+void XorGate::handleMessage(cMessage *msg)
+{
+    if(msg->arrivedOn("in1")){
+      in1 = static_cast<bitmessage *>(msg)->getSource();
+      count++;
+    }
+    else if(msg->arrivedOn("in2")){
+           in2 = static_cast<bitmessage *>(msg)->getSource();
+           count++;
+    }
+    if(count ==2){
+    static_cast<bitmessage *>(msg)->setSource(in1^in2);
+    send(msg,"out");
+    count = 0;
+    }
+}
